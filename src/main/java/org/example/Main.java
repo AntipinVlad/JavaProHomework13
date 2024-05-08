00ffc3374fdc4768a039d78a50c8a27d590186f5
AntipinVlad <antipinmail1@gmail.com>
2024-05-08 12:53:23 +0300

    created simple calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,46 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 3) {
+            System.out.println("Enter 3 elements: first number, operator, second number");
+            return;
+        }
+
+        double firstNumber, secondNumber;
+        String operator = args[1];
+
+        try {
+            firstNumber = Double.parseDouble(args[0]);
+            secondNumber = Double.parseDouble(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid number format: " + e.getMessage());
+            return;
+        }
+
+        double result;
+
+        switch (operator) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                if (secondNumber == 0) {
+                    System.out.println("We can't divide by zero");
+                    return;
+                }
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.printf("%.2f%s%.2f=%.2f", firstNumber, operator, secondNumber, result);
     }
 }
